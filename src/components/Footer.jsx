0d6d912d8e2faa966da19a0d3d4c23d2d538bbdb
Nimish <nimish>
2023-03-04 14:04:06 +0530

    added overview

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -198,17 +198,11 @@ function SmallPrint() {
   return (
     <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
       <p className="text-xs text-zinc-600 dark:text-zinc-400">
-        &copy; Copyright {new Date().getFullYear()}. All rights reserved.
+        &copy; Copyright Phase {new Date().getFullYear()}. All rights reserved.
       </p>
       <div className="flex gap-4">
-        <SocialLink href="#" icon={TwitterIcon}>
-          Follow us on Twitter
-        </SocialLink>
-        <SocialLink href="#" icon={GitHubIcon}>
+        <SocialLink href="https://github.com/phasehq" icon={GitHubIcon}>
           Follow us on GitHub
-        </SocialLink>
-        <SocialLink href="#" icon={DiscordIcon}>
-          Join our Discord server
         </SocialLink>
       </div>
     </div>
