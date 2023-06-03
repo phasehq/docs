4216f2f639893211f5c71cc3be5e0e6ea808d525
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-06-03 19:09:14 +0530

    feat: add next-seo, new meta image

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Router, useRouter } from 'next/router'
 import { MDXProvider } from '@mdx-js/react'
-
+import { NextSeo } from 'next-seo';
 import { Layout } from '@/components/Layout'
 import * as mdxComponents from '@/components/mdx'
 import { useMobileNavigationStore } from '@/components/MobileNavigation'
@@ -29,6 +29,26 @@ export default function App({ Component, pageProps }) {
         )}
         <meta name="description" content={pageProps.description} />
       </Head>
+      <NextSeo
+        title={router.pathname === '/' ? 'Phase Docs' : `${pageProps.title} - Phase Docs`}
+        description='Phase Documentation'
+        openGraph={{
+          title: router.pathname === '/' ? 'Phase Docs' : `${pageProps.title} - Phase Docs`,
+          description: pageProps.description,
+          url: window.location.href,
+          locale: 'en',
+          site_name: 'Phase DOCS',
+          images: [
+            {
+              url: `${router.basePath}/assets/images/docs-meta.png`,
+              width: 1200,
+              height: 630,
+              alt: 'docs.hase.dev',
+              type: 'image/png',
+            },
+          ],
+        }}
+      />
       <MDXProvider components={mdxComponents}>
         <Layout {...pageProps}>
           <Component {...pageProps} />
