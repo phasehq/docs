2d71d29dd85b6b6602ec6c70b49378b355fa94eb
Nimish <nimish@phase.dev>
2023-08-29 12:03:55 +0530

    more frameworks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -219,8 +219,7 @@ function SmallPrint() {
   return (
     <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
       <p className="text-xs text-zinc-600 dark:text-zinc-400">
-        &copy; Copyright {new Date().getFullYear()} Phase Security Inc. All
-        rights reserved.
+        &copy; {new Date().getFullYear()} Phase Security Inc.
       </p>
       <div className="flex gap-4">
         <SocialLink href="https://github.com/phasehq/console" icon={GitHubIcon}>
