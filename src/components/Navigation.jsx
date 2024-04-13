1b349b6ab801785bf17911e48899da6b4e5c022a
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-04-13 15:19:58 +0530

    wip: api docs

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -325,6 +325,15 @@ export const navigation = [
     ],
   },
   {
+    title: 'API',
+    links: [
+      { title: 'Overview', href: '/public-api' },
+      { title: 'Encryption', href: '/public-api/encryption' },
+      { title: 'Authentication', href: '/public-api/authentication' },
+      { title: 'Secrets', href: '/public-api/secrets' },
+    ],
+  },
+  {
     title: 'Security',
     links: [
       { title: 'Overview', href: '/security' },
