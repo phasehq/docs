5b8422246faca92ca38f471ddb0307c83c6ce17c
Nimish <nimish@phase.dev>
2024-06-12 13:23:21 +0530

    feat: improve clidemo component to take castfile and fontsize

--- src/components/CliDemo.jsx
+++ src/components/CliDemo.jsx
@@ -1,23 +1,26 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
-const CliDemo = () => {
+const CliDemo = ({ castFile, terminalFontSize}) => {
   const router = useRouter();
   const [playerInitialized, setPlayerInitialized] = useState(false);
+
+  // Generate a unique ID for the player container
+  const uniqueId = `player-container-${Math.random().toString(36).substr(2, 9)}`;
 
   useEffect(() => {
     const initializeAsciinema = () => {
       if (window.AsciinemaPlayer && !playerInitialized) {
         window.AsciinemaPlayer.create(
-          `${router.basePath}/assets/asciinema/phase-cli-demo.cast`,
-          document.getElementById('player-container'),
+          `${router.basePath}/assets/asciinema/${castFile}`,
+          document.getElementById(uniqueId),
           {
-            // cols: 96,
+            //cols: 150,
             rows: 35,
             autoplay: true,
             loop: true,
             fit: false,
-            // terminalFontSize: 'medium',
+            terminalFontSize: terminalFontSize,
           }
         );
         setPlayerInitialized(true);
@@ -33,15 +36,15 @@ const CliDemo = () => {
     return () => {
       window.removeEventListener('load', initializeAsciinema);
     };
-  }, []);
+  }, [castFile, terminalFontSize, playerInitialized, router.basePath, uniqueId]);
 
   return (
     <div>
-    <div
-        id="player-container"
+      <div
+        id={uniqueId}
         className="max-w-full text-[9px] sm:text-sm md:text-base xl:text-lg"
       ></div>
-      </div>
+    </div>
   );
 };
 
