84d584c0afc7dff1d7fb5f1f53dc20d555ca7c98
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-06-24 15:29:35 +0530

    feat: misc fixes

--- src/components/ZoomableImage.jsx
+++ src/components/ZoomableImage.jsx
@@ -1,12 +1,10 @@
 import Zoom from 'react-medium-image-zoom';
-import 'react-medium-image-zoom/dist/styles.css';
 import Image from 'next/image';
-import styles from '@/styles/ZoomableImage.module.css';
 
 const ZoomableImage = ({ src, alt, width, height }) => (
-  <div className={styles.imageWrapper}>
-    <Zoom overlayBgColorEnd="rgba(0, 0, 0, 0.9)" classDialog={styles.zoomOverlay}>
-      <Image src={src} alt={alt} width={width} height={height} layout="intrinsic" />
+  <div className="inline-block mr-5">
+    <Zoom zoomMargin={45}>
+      <Image src={src} alt={alt} width={width} height={height} layout="intrinsic" className="rounded-lg" />
     </Zoom>
   </div>
 );
