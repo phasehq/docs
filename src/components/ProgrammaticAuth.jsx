4c380b67b190af76dfcb20a9e37eaf1765457261
Nimish <nimish@phase.dev>
2024-12-09 16:42:01 +0530

    feat: added programmatic component in access control

--- src/components/ProgrammaticAuth.jsx
+++ src/components/ProgrammaticAuth.jsx
@@ -22,7 +22,7 @@ export function ProgrammaticAuth() {
   return (
     <div className="my-16 xl:max-w-none">
       <Heading level={2} id="guides">
-        Token Types
+        Access Token Types
       </Heading>
       <div className="not-prose mt-4 grid grid-cols-1 gap-8 border-t border-zinc-900/10 pt-10 dark:border-white/5 sm:grid-cols-2 xl:grid-cols-4">
         {resources.map((resource) => (
